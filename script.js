641d0e066e10019d82b37e027e2388edd72d3aef
CrazymeJe <russo95.jessica@gmail.com>
2020-02-04 15:58:53 +0100

    finito

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ $(".iscriviti").click (function () {
     $("#nascondi").removeClass("nascondi");
     $("#nascondi").addClass("blocco");
     $("body").addClass("no-scroll");
+
+});
+$(".close").click( function (){
+    $("#nascondi").addClass("nascondi");
+    $("#nascondi").removeClass("blocco");
+    $("body").removeClass("no-scroll");
+
 });
